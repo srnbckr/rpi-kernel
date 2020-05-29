728aef6f9ae69d97e6601bce4d4f584abaa19cad
Soeren Becker <soeren.becker@tu-berlin.de>
2020-05-29 14:43:55 +0200

    Add some packages to prepare-build-environment

--- scripts/prepare_build_environment.sh
+++ scripts/prepare_build_environment.sh
@@ -3,6 +3,6 @@
 apt-get update
 
 # needed by compile-kernel
-for package in git-core build-essential libncurses5-dev bc tree fakeroot devscripts binfmt-support qemu qemu-user-static debootstrap kpartx lvm2 dosfstools apt-cacher-ng debhelper quilt; do
+for package in bison flex libssl-dev git-core build-essential libncurses5-dev bc tree fakeroot devscripts binfmt-support qemu qemu-user-static debootstrap kpartx lvm2 dosfstools apt-cacher-ng debhelper quilt; do
   apt-get install -y $package
 done
